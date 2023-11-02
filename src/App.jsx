ddebf99c9090d12e7135c87bba37974efb96357a
sanin-mn <msanin7003@gmail.com>
2023-11-02 13:39:54 +0530

    ui and qustions completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,61 @@
+import React, { useState } from 'react'
 import './App.css'
+import questions from './questionsList'
 
 function App() {
 
+  const [showResult, setShowResult] = useState(false);
+  const [score,setScore] = useState(0);
+  const [currentQuestion,setCurrentQuestion] = useState(1);
+
+
+
+  const optionClicked = (isCorrect)=>{
+    if (isCorrect){
+      setScore(score+1)
+    }
+    if(currentQuestion + 1 < questions.length){
+      setCurrentQuestion(currentQuestion+1)
+    }else{
+      setShowResult(true)
+    }
+  }
+  const restartGame = ()=>{
+    setScore(0);
+    setCurrentQuestion(0);
+    setShowResult(false)
+  }
+
+
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <div className='App'>
+      <h1 className='pt-5 text-success'>Coding Quiz</h1>
+    <h2 className='d-inline mt-4' style={{color:'#fef65b'}}>Current score : {score}</h2>
+
+      {showResult ?
+        <div className='result'>
+          <h3 className='text-danger'>Final result</h3>
+          <h5>{score} out of {questions.length} correct ({(score/questions.length)*100}%)</h5>
+          <button onClick={()=>restartGame()} className='btn btn-success'>Restart Game</button>
+        </div>
+        :
+        <div className="box">
+          <h5 className='text-success'>Question {currentQuestion +1} out of {questions.length}</h5>
+          <h3 className='question mt-3'>{questions[currentQuestion].text}</h3>
+
+          <ul className='mt-4 list-inline'>
+              {questions[currentQuestion].options.map((option)=>
+                (
+                  
+                  <li className='list-inline-item me-3' onClick={()=> optionClicked(option.isCorrect)} key={option.id}>{option.text}</li>
+                  
+                )
+              )}
+          </ul>
+        </div>
+      }
+
+    </div>
   )
 }
 
---
+++ src/questionsList.js
@@ -0,0 +1,49 @@
+ const questions = [
+    {
+      text: " Which type of JavaScript language is ___?",
+      options: [
+        { id: 0, text: "Object-Oriented", isCorrect: false },
+        { id: 1, text: "Assembly-language", isCorrect: false },
+        { id: 2, text: "High-level", isCorrect: false },
+        { id: 3, text: "Object-Based", isCorrect: true },
+      ],
+    },
+    {
+      text: "What does CSS stand for?",
+      options: [
+        { id: 0, text: "Cascading style sheet", isCorrect: true },
+        { id: 1, text: "Colorful style sheet", isCorrect: false },
+        { id: 2, text: "Common style sheet", isCorrect: false },
+        { id: 3, text: "Computer style sheet", isCorrect: false },
+      ],
+    },
+    {
+      text: "Which HTML attribute is used to reference an external Javascript file?",
+      options: [
+        { id: 0, text: "<src>", isCorrect: true },
+        { id: 1, text: "<p>", isCorrect: false },
+        { id: 2, text: "<footer>", isCorrect: false },
+        { id: 3, text: "<Java>", isCorrect: false },
+      ],
+    },
+    {
+      text: "The function and  var are known as?",
+      options: [
+        { id: 0, text: "Keywords", isCorrect: false },
+        { id: 1, text: "Declaration statements", isCorrect: true },
+        { id: 2, text: "Prototypes", isCorrect: false },
+        { id: 3, text: "Data types", isCorrect: false },
+      ],
+    },
+    {
+      text: "Which one of the following operator returns false if both values are equal?",
+      options: [
+        { id: 0, text: "!", isCorrect: false },
+        { id: 1, text: "!==", isCorrect: true },
+        { id: 2, text: "!=", isCorrect: true },
+        { id: 3, text: "All of the above", isCorrect: false },
+      ],
+    },
+  ];
+
+  export default questions
